97c285060f22b4612e2953e148ada4b75b1bc8b8
Yekta Kalantary <26902978+yekta-kalantary@users.noreply.github.com>
2025-05-30 13:26:14 +0330

    fix Identifier name too long for wallet transactionable

--- database/migrations/2025_05_30_081617_create_wallet_transactions_table.php
+++ database/migrations/2025_05_30_081617_create_wallet_transactions_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->foreignIdFor(Wallet::class, 'wallet_id')->constrained();
             $table->decimal('amount', 16, 3);
             $table->text('description');
-            $table->nullableMorphs('transactionable');
+            $table->nullableMorphs('transactionable' , 'trx');
             $table->timestamp('created_at');
         });
     }
